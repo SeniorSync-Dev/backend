33bfc35aa48da0a6b951dd5645d482071431c11a
Alexander Colmorn <Alex802c@gmail.com>
2026-09-08 16:19:52 +0200

    added mitid integration for simplicity

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,22 +2,53 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from '../db/schemas';
 import dbClient from "../db/dbClient";
-import { admin } from "better-auth/plugins"
+import { admin, genericOAuth } from "better-auth/plugins"
 
 export const auth = betterAuth({
-  baseURL: process.env.AUTH_BASE_URL || "http://localhost:3000",
-  emailAndPassword: {
-        enabled: true,
+    baseURL: process.env.AUTH_BASE_URL || "http://localhost:3000",
+    emailAndPassword: {
+        enabled: false,
     },
 
-  database: drizzleAdapter(dbClient, {
+    database: drizzleAdapter(dbClient, {
         provider: "pg",
         schema: schema,
     }),
 
     plugins: [
-        admin() 
+        admin(),
+        genericOAuth({
+            config: [
+                {
+                    providerId: "mitid",
+                    clientId: "sandbox-pompous-blade-464",
+                    clientSecret: "YhScfzg326C9mmYS7qLfMSPhEwKHPqz4bK0xLoIVtsaPPE9c",
+                    discoveryUrl: "https://seniorsync.sandbox.signicat.com/auth/open/.well-known/openid-configuration",
+                    scopes: [
+                        "openid",
+                        "profile",
+                        "email",
+                        "nin",
+                        "mitid-extra",
+                    ],
+                    mapProfileToUser: (profile) => {
+                        console.log("SIGNICAT PROFILE:");
+                        console.dir(profile, { depth: null });
+
+                        return {
+                            name: profile.name ??
+                                `${profile.given_name ?? ""} ${profile.family_name ?? ""}`.trim(),
+
+                            email:
+                                profile.email ??
+                                `${profile.sub}@mitid.invalid`,
+                        };
+                    },
+                },
+                // Add more providers as needed
+            ]
+        })
     ],
 
-     trustedOrigins: ["http://localhost:3000", "http://localhost:3001"],
+    trustedOrigins: ["http://localhost:3000", "http://localhost:3001"],
 });
