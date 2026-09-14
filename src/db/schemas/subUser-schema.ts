c20c1cefb600dcc78177d984c808f132d9089507
Sander Petersen <ezaguegaming@gmail.com>
2026-09-14 13:35:39 +0200

    Update src/db/schemas/subUser-schema.ts
    Co-authored-by: Alexander Colmorn <62727649+V4LVE@users.noreply.github.com>

--- src/db/schemas/subUser-schema.ts
+++ src/db/schemas/subUser-schema.ts
@@ -72,7 +72,7 @@ export const employee = pgTable("employee", {
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at")
         .defaultNow()
-        .$onUpdate(() => /* @__PURE__ */ new Date())
+        .$onUpdate(() => new Date())
         .notNull(),
 });
 
